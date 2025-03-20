017a0e1aa5170ff51ba7879dfd17824eed5f816a
Kanchan <kanchan96437@gmail.com>
2025-03-20 23:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const select = document.getElementById("colorSelect");
+    const button = document.querySelector("input[type='button']");
+
+    button.addEventListener("click", function () {
+        if (select.selectedIndex !== -1) {
+            select.remove(select.selectedIndex);
+        }
+    });
+});
